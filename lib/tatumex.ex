f6ee9fe28c5f24a9cf0e5dcf6af455986f7c4f52
Ruben Murga <ruben.murga.d@gmail.com>
2021-08-31 19:57:42 -0500

    make request

--- lib/tatumex.ex
+++ lib/tatumex.ex
@@ -26,18 +26,22 @@ defmodule Tatumex do
     "#{Application.get_env(:tatumex, :tatum_url)}/#{currency_string}/#{endpoint}/#{extra}"
   end
 
-  def create_wallet(currency) do
-    url = create_url currency, "wallet"
+  defp make_request(key, url)  do
     {:ok, %HTTPoison.Response{body: body}} = HTTPoison.get(url, create_headers())
     {:ok, body_decoded} = Poison.decode(body)
-    {:ok, body_decoded["xpub"]}
+    {:ok, body_decoded[key]}
+  end
+
+  def create_wallet(currency) do
+    url = create_url currency, "wallet"
+    {:ok, value} = make_request "xpub", url
+    {:ok, value}
   end
 
   def create_address(currency) do
     {:ok, xpub} = create_wallet currency
     url = create_url currency, "address", "#{xpub}/1"
-    {:ok, %HTTPoison.Response{body: body}} = HTTPoison.get(url, create_headers())
-    {:ok, body_decoded} = Poison.decode(body)
-    {:ok, body_decoded["address"]}
+    {:ok, value } = make_request "address", url
+    {:ok, value}
   end
 end
